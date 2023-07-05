13cbb3acad4a6c0b32fa0a3fad17b4c16b5baed3
trieu <tantrieuf31.database@gmail.com>
2023-07-05 19:09:59 +0700

    add test data + use pandas for CLV

--- compute_clv.py
+++ compute_clv.py
@@ -5,10 +5,33 @@ import datetime
 import pandas as pd
 from datetime import datetime as dt
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
 CUSTOMER_ID = 0
 DATE = 1
 AMOUNT = 2
 LIFETIME_ONE_DAY = 1000 * 60 * 60 * 24
+
+def to_timestamp(s):
+    return time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple()) * 1000
+
+# array of [customer ID, transaction-date, total-value-of-order]
+orders = [
+    [1, to_timestamp("01/03/2023"), 100],
+    [1, to_timestamp("30/04/2023"), 200],
+    [1, to_timestamp("05/07/2023"), 150],
+    [2, to_timestamp("15/03/2023"), 300],
+    [2, to_timestamp("15/06/2023"), 400],
+    [2, to_timestamp("29/06/2023"), 300],
+    [3, to_timestamp("20/01/2023"), 500],
+    [3, to_timestamp("20/06/2023"), 200],
+    [4, to_timestamp("30/06/2023"), 100]
+]
+
+df_header = ['Customer ID', 'AVG Order Size', 'AVG Order Frequency',
+                  'AVG Customer Value', 'AVG Customer Lifespan', 'CLV']
+
 
 # This code is about how to compute CLV https://en.wikipedia.org/wiki/Customer_lifetime_value
 # Video about CLV: https://www.youtube.com/watch?v=JR8_GNINmuQ
@@ -17,43 +40,35 @@ LIFETIME_ONE_DAY = 1000 * 60 * 60 * 24
 ''' 
   Groups customers by their ID calculates customer lifetime value for each.
  
-  @param {Any[][]} rows - array of customer ID, date, amount.
-  @returns {Any[][]} - table, each row containing:
-    - average order size
+  @param {pandas.DataFrame} - DataFrame of customer ID, date, amount.
+  @returns {pandas.DataFrame} - DataFrame, each row containing:
+    - customer id,
+    - average order size,
     - avgOrderFrequency,
     - avgCustomerValue,
     - avgCustomerLifespan,
-    - customerLifetimeValue,
-    - map of id:orders
+    - customerLifetimeValue
 '''
-
-
-def lifetime_values(rows, product_lifetime):
-    orders_per_customer = group_by_key(rows)
-
+def lifetime_values(df, product_lifetime):
     result = []
-    for id, orders in orders_per_customer.items():
-        total_revenue = sum(order['amount'] for order in orders)
-
-        dates_range = {'first': None, 'last': None}
-        for order in orders:
-            date = order['date']
-            dates_range['first'] = min(dates_range['first'] or date, date)
-            dates_range['last'] = max(dates_range['last'] or date, date)
+    
+    for id, group in df.groupby('customer_id'):
+        total_revenue = group['amount'].sum()
+        dates_range = {'first': group['date'].min(), 'last': group['date'].max()}
 
         # convert product_lifecycle to product_lifetime_in_milisecs
         product_lifetime_in_milisecs = LIFETIME_ONE_DAY * product_lifetime
+
         # compute total_months as customer_lifetime
-        total_months = (
-            dates_range['last'] - dates_range['first']) / product_lifetime_in_milisecs
+        total_months = (dates_range['last'] - dates_range['first']) / product_lifetime_in_milisecs
         if total_months == 0:
             total_months = 1
 
-        avg_order_size = total_revenue / len(orders)
-        avg_order_frequency = len(orders) / len(orders_per_customer)
+        avg_order_size = total_revenue / len(group)
+        avg_order_frequency = len(group) / df['customer_id'].nunique()
         avg_customer_value = avg_order_size * avg_order_frequency
-        avg_product_lifetime_in_milisecs = total_months
-        customer_lifetime_value = avg_customer_value * avg_product_lifetime_in_milisecs
+        avg_customer_lifespan = total_months
+        customer_lifetime_value = avg_customer_value * avg_customer_lifespan
 
         if customer_lifetime_value == 0:
             avg_customer_value *= 0.1
@@ -63,37 +78,13 @@ def lifetime_values(rows, product_lifetime):
             avg_order_size,
             avg_order_frequency,
             avg_customer_value,
-            avg_product_lifetime_in_milisecs,
+            avg_customer_lifespan,
             customer_lifetime_value
         ])
-    return result
-
-
-def group_by_key(key_values):
-    key_values_map = defaultdict(list)
-    for key_value in key_values:
-        key, value = key_value[CUSTOMER_ID], {
-            'date': key_value[DATE], 'amount': key_value[AMOUNT]}
-        key_values_map[key].append(value)
-    return key_values_map
-
-
-def to_timestamp(s):
-    return time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple()) * 1000
+        
+    return pd.DataFrame(result, columns=df_header)
 
 # test
-
-
-# array of [customer ID, transaction-date, total-value-of-order]
-orders = [
-    [1, to_timestamp("01/06/2023"), 100],
-    [1, to_timestamp("30/06/2023"), 200],
-    [2, to_timestamp("15/01/2023"), 300],
-    [2, to_timestamp("15/06/2023"), 400],
-    [3, to_timestamp("20/05/2023"), 500],
-    [3, to_timestamp("20/06/2023"), 200],
-    [4, to_timestamp("30/06/2023"), 100]
-]
 
 df = pd.DataFrame(orders, columns=['Customer ID', 'Order Date', 'Total Value'])
 df['Order Date'] = df['Order Date'].map(lambda t: dt.fromtimestamp(t / 1000))
@@ -102,11 +93,7 @@ print(df)
 
 # after 30 days, the product could be purchased again
 product_lifetime = 30
-result = lifetime_values(orders, product_lifetime)
-resultMetadata = ['Customer ID', 'Average Order Size', 'Average Order Frequency',
-                  'Average Customer Value', 'Average Customer Lifespan', 'Customer Lifetime Value']
-
-dfResult = pd.DataFrame(result, columns=resultMetadata)
-print('\n Results:')
+df_orders = pd.DataFrame(orders, columns=['customer_id', 'date', 'amount'])
+dfResult = lifetime_values(df_orders, product_lifetime)
+print('\n dfResult:')
 print(dfResult)
-
--- test_dask.py
+++ test_dask.py
@@ -5,10 +5,23 @@ import dask.dataframe as dd
 import dask.array as da
 import dask.bag as db
 
-index = pd.date_range("2021-09-01", periods=2400, freq="1H")
-df = pd.DataFrame({"a": np.arange(2400), "b": list("abcaddbe" * 300)}, index=index)
-ddf = dd.from_pandas(df, npartitions=10)
-rs = ddf["2021-10-01": "2021-10-09 5:00"].compute()
-print(ddf)
-print(rs)
-print(ddf.a.mean().compute())
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+# download file at https://drive.google.com/file/d/1g7LyuJFRzBFeWBbmrirGozxjCy61Cvyc/view
+dfx = pd.DataFrame(pd.read_excel("online_retail_II.xlsx"))
+ddf = dd.from_pandas(dfx, npartitions=10)
+
+result = ddf.head(100)
+print("First 10 rows of the DataFrame:")
+print(result)
+  
+
+def test():
+    index = pd.date_range("2021-09-01", periods=2400, freq="1H")
+    df = pd.DataFrame({"a": np.arange(2400), "b": list("abcaddbe" * 300)}, index=index)
+    ddf = dd.from_pandas(df, npartitions=10)
+    rs = ddf["2021-10-01": "2021-10-09 5:00"].compute()
+    print(ddf)
+    print(rs)
+    print(ddf.a.mean().compute())
