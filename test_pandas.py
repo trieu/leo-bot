711cc2330dce1dbeea3caa9d0190f26fa41a8955
trieu <tantrieuf31.database@gmail.com>
2023-07-10 19:39:42 +0700

    add Total Revenue, add pyarango

--- test_pandas.py
+++ test_pandas.py
@@ -10,7 +10,7 @@ pd.set_option('display.max_rows', None)
 
 default_profit_margin = 0.1
 LIFETIME_ONE_DAY = 1000 * 60 * 60 * 24
-df_header = ['AVG Order Size', 'AVG Order Frequency', 'AVG Customer Value', 'AVG Customer Lifespan', 'CLV']
+df_header = ['Total Revenue','AVG Order Size', 'AVG Order Frequency', 'AVG Customer Value', 'AVG Customer Lifespan', 'CLV']
 
 def to_timestamp(s):
     return time.mktime(datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S").timetuple()) * 1000
@@ -18,7 +18,7 @@ def to_timestamp(s):
 # Video for CLV explanation: https://www.youtube.com/watch?v=JR8_GNINmuQ
 def compute_clv(df, purchasing_lifecycle = 30, first_purchasing_date = 0, last_purchasing_date = 0):
     results = []
-    total_revenue = df['Total Price'].sum()
+    total_revenue = df['Total Revenue'].sum()
     dates_range = {'first': to_timestamp(first_purchasing_date), 'last': to_timestamp(last_purchasing_date) }
 
     # convert purchasing_lifecycle to purchasing_lifecycle_in_milisecs
@@ -36,9 +36,10 @@ def compute_clv(df, purchasing_lifecycle = 30, first_purchasing_date = 0, last_p
     customer_lifetime_value = avg_customer_value * avg_customer_lifespan
 
     if customer_lifetime_value == 0:
-        avg_customer_value *= 0.1
+        total_revenue *= 0.1
 
     results.append([
+        total_revenue,
         avg_order_size,
         avg_order_frequency,
         avg_customer_value,
@@ -48,17 +49,22 @@ def compute_clv(df, purchasing_lifecycle = 30, first_purchasing_date = 0, last_p
     return pd.DataFrame(results, columns=df_header)
 
 def processData(groupData):
-    total_price = groupData['Price'].astype(float) * groupData['Quantity'].astype(float)
-    groupData['Total Price'] = total_price * (1 - default_profit_margin)
+    customer_id = ','.join(str(e) for e in groupData['Customer ID'].unique())
+    # assumption: after 7 days,  a customer has a 100% chance of returning to your store
+    purchasing_lifecycle = 7 
+    # the price of each product mulitply with the number of quantity
+    total_revenue = groupData['Price'].astype(float) * groupData['Quantity'].astype(float)
+    groupData['Total Revenue'] = total_revenue * (1 - default_profit_margin)
+
+    # time range
     first_purchasing_date = groupData['InvoiceDate'].min()
     last_purchasing_date = groupData['InvoiceDate'].max()
-    customer_id = ','.join(str(e) for e in groupData['Customer ID'].unique())
     
     print('\n ')
     # print(groupData)
     print('Customer ID = {0} Transaction Date from [{1}] to [{2}]'.format(customer_id, first_purchasing_date, last_purchasing_date))      
 
-    clv_dataframe = compute_clv(groupData, 30, first_purchasing_date, last_purchasing_date)
+    clv_dataframe = compute_clv(groupData, purchasing_lifecycle, first_purchasing_date, last_purchasing_date)
     print(clv_dataframe.to_markdown())
     return 1
 
@@ -80,4 +86,4 @@ df_result = sample.loc[sample['Customer ID'].isin(filter_customer_ids)].groupby(
 # loop in dataframe to compute CLV for each customer
 total_customer = df_result.apply(processData).sum()
    
-print('\n =>> Total {0} Customer'.format(total_customer)) 
+print('\n =>> Total {0} Customer'.format(total_customer))
