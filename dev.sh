ff261ab9966ccdaabfd1d18cbc14651dd0bbd865
trieu <tantrieuf31.database@gmail.com>
2023-10-24 18:57:55 +0700

    update dev sh

--- dev.sh
+++ dev.sh
@@ -2,9 +2,13 @@
 
 git pull
 
+sleep 1
+
 # Activate your virtual environment if necessary
 SOURCE_PATH="env/bin/activate"
 source $SOURCE_PATH
+
+sleep 1
 
 # Start the FastAPI app using uvicorn
 uvicorn main:leobot --reload --env-file .env --host 0.0.0.0 --port 8888 
