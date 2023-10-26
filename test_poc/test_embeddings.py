f7f07fb77f2f760ac29f6b69af26fa93caac450b
trieu <tantrieuf31.database@gmail.com>
2023-10-26 21:03:20 +0700

    update test embedding

--- test_poc/test_embeddings.py
+++ test_poc/test_embeddings.py
@@ -1,10 +1,20 @@
-
 import numpy as np
 
+'''
+Trong học máy, một embedding là một đại diện vector cho một đối tượng. 
+Nó là một cách để mã hóa thông tin về đối tượng thành một vector có thể được sử dụng bởi các mô hình học máy.
+Embedding thường được sử dụng để đại diện cho các từ hoặc cụm từ trong ngôn ngữ tự nhiên. 
+Ví dụ, một embedding cho từ "cat" có thể là một vector có các thành phần đại diện cho các thuộc tính của con mèo, chẳng hạn như kích thước, màu sắc và loài.
+'''
 def embed(word:str ):
   return np.array([len(word), word.count("a"), word.count("b"), word.count("c")])
-
-print(embed("cat"))
+'''
+Đoạn mã trên định nghĩa một hàm embed() ánh xạ một từ thành một vector có hai thành phần. 
+Thành phần đầu tiên đại diện cho độ dài của từ, 
+trong khi thành phần thứ hai đại diện cho số lần chữ "e" xuất hiện trong từ. 
+Đây là một embedding vì nó đại diện cho các thuộc tính của từ.
+'''
+print(embed("cat")) # [3 1 0 1]
 
 from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer('sentence-transformers/msmarco-distilroberta-base-v2')
@@ -18,7 +28,7 @@ embeddings = model.encode(knowledges)
 #Print the embeddings
 for sentence, embedding in zip(knowledges, embeddings):
     print("Sentence:", sentence)
-    print("Embedding:", embedding)
+    print("Embedding.shape: " + str(embedding.shape[0]) )
     print("")
 
 question = 'What does kitten eat ?'
@@ -29,9 +39,9 @@ knowledges_database_embedding = model.encode(knowledges_database)
 
 print("Similarity:", util.dot_score(query_embedding, knowledges_database_embedding))
 
+print("question: \"", question, "\" and the answers: \n ")
 hits = util.semantic_search(query_embedding, knowledges_database_embedding, top_k=2)
 hits = hits[0]      #Get the hits for the first query
 for hit in hits:
-    id = hit['corpus_id']
-    print("corpus_id: ", id)
-    print(knowledges_database[id], "(Score: {:.4f})".format(hit['score']))
+    id = hit['corpus_id']   
+    print(knowledges_database[id], " (ID: {:g}) (Score: {:.4f})".format(id, hit['score']))
