5f7599e8f74b0a62c887b370a7d29cc8868bf49c
trieu <tantrieuf31.database@gmail.com>
2023-10-26 20:46:59 +0700

    update test embedding

--- test_poc/test_embeddings.py
+++ test_poc/test_embeddings.py
@@ -1,24 +1,37 @@
 
-from sentence_transformers import SentenceTransformer, util
+import numpy as np
 
+def embed(word:str ):
+  return np.array([len(word), word.count("a"), word.count("b"), word.count("c")])
+
+print(embed("cat"))
+
+from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer('sentence-transformers/msmarco-distilroberta-base-v2')
 
-#Our sentences we like to encode
-sentences = ['This framework generates embeddings for each input sentence',
-    'Sentences are passed as a list of string.',
-    'The quick brown fox jumps over the lazy dog.']
+#Our knowledges we like to encode
+knowledges = ['I love cat', 'Cat hates dog', 'Kitty is a small cat']
 
 #Sentences are encoded by calling model.encode()
-embeddings = model.encode(sentences)
+embeddings = model.encode(knowledges)
 
 #Print the embeddings
-for sentence, embedding in zip(sentences, embeddings):
+for sentence, embedding in zip(knowledges, embeddings):
     print("Sentence:", sentence)
     print("Embedding:", embedding)
     print("")
 
-query_embedding = model.encode('How big is London')
-passage_embedding = model.encode(['London has 9,787,426 inhabitants at the 2011 census',
-                                  'London is known for its finacial district'])
+question = 'What does kitten eat ?'
+query_embedding = model.encode(question)
 
-print("Similarity:", util.dot_score(query_embedding, passage_embedding))
+knowledges_database = knowledges + ['The 2 kittens are gray', '1 kitten is yellow', 'All kittens love to eat chicken pate']
+knowledges_database_embedding = model.encode(knowledges_database)
+
+print("Similarity:", util.dot_score(query_embedding, knowledges_database_embedding))
+
+hits = util.semantic_search(query_embedding, knowledges_database_embedding, top_k=2)
+hits = hits[0]      #Get the hits for the first query
+for hit in hits:
+    id = hit['corpus_id']
+    print("corpus_id: ", id)
+    print(knowledges_database[id], "(Score: {:.4f})".format(hit['score']))
