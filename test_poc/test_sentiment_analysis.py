cc8b568747c52632df5473a2aa8228517ff8767e
Trieu Nguyen <tantrieuf31.database@gmail.com>
2023-11-06 08:07:59 +0700

    update sentiment test

--- test_poc/test_sentiment_analysis.py
+++ test_poc/test_sentiment_analysis.py
@@ -16,9 +16,11 @@ text_en = lang_translation_pipe(text_vi)
 print(text_en)
 print(sentiment_pipe(text_en[0]['translation_text']))
 
-
-#text = "As a product executive of more than 15 years, I find a lot of this lean product stuff to miss the mark. The book is okay for what it is, but lean product management doesn't really work well in practice except in particular UI based applications. And even then, companies that practice lean product management do a pretty terrible job with their interfaces given the amount of resources they have: examples in point: Amazon.com, Ebay, and Netflix."
-#print(sentiment_pipe(text))
+text = '''As a product executive of more than 15 years, I find a lot of this lean product stuff to miss the mark. 
+The book is okay for what it is, but lean product management doesn't really work well in practice except in particular UI based applications. 
+And even then, companies that practice lean product management do a pretty terrible job 
+with their interfaces given the amount of resources they have: examples in point: Amazon.com, Ebay, and Netflix.'''
+print(sentiment_pipe(text))
 
 #lang_detection_pipe = pipeline("text-classification", model="papluca/xlm-roberta-base-language-detection", device=0)
 #print(lang_detection_pipe(text_vn))
