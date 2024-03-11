b6023910d07997d08b25311f2811edd1a0c1c8d9
trieu <tantrieuf31.database@gmail.com>
2024-03-11 15:53:49 +0700

    fix and update qdrant test

--- test_poc/test_semantic_search.py
+++ test_poc/test_semantic_search.py
@@ -1,4 +1,4 @@
-from sentence_transformers import SentenceTransformer, util
+from sentence_transformers import SentenceTransformer
 
 import os
 import time
@@ -9,31 +9,30 @@ from qdrant_client.http.models import PointStruct
 
 # https://huggingface.co/sentence-transformers/msmarco-distilroberta-base-v2
 MODEL_NAME = 'sentence-transformers/msmarco-distilroberta-base-v2'
-VECTOR_DIM_SIZE = 768 # the size of msmarco-distilroberta-base-v2
+
 model = SentenceTransformer(MODEL_NAME)
+VECTOR_DIM_SIZE = model.get_sentence_embedding_dimension() # the size of msmarco-distilroberta-base-v2
 
 qdrantClient = QdrantClient("localhost", port=6333)
 qdrantClient.recreate_collection(
-    collection_name="test_collection",
-    vectors_config=VectorParams(size=VECTOR_DIM_SIZE, distance=Distance.DOT),
+    collection_name="text_data",
+    vectors_config=VectorParams(size=VECTOR_DIM_SIZE, distance=Distance.COSINE),
 )
 
 # Corpus with example sentences
-corpus_list = ['A man is eating food.',
-          'A man is eating a piece of bread.',
-          'The girl is carrying a baby.',
-          'A man is riding a horse.',
-          'A woman is playing violin.',
-          'Two men pushed carts through the woods.',
-          'A man is riding a white horse on an enclosed ground.',
-          'A monkey is playing drums.',
-          'A cheetah is running behind its prey.',
-          'God is love.',
-          'Love is everywhere.'
+corpus_list = [
+          'Một người đàn ông đang ăn thức ăn.',
+           'Một người đàn ông đang ăn một miếng bánh mì.',
+           'Cô gái đang bế một đứa bé.',
+           'Một người đàn ông đang cưỡi ngựa.',
+           'Một người phụ nữ đang chơi violin.',
+           'Hai người đàn ông đẩy xe xuyên rừng.',
+           'Một người đàn ông đang cưỡi một con ngựa trắng trên một khu đất kín.',
+           'Một con khỉ đang chơi trống.',
+           'Một con báo đang chạy theo con mồi.',
+           'Gia đình là tình yêu.',
+           'Tình yêu ở mọi nơi.'
           ]
-
-corpus_ids = []
-corpus_embeddings = []
 
 start = time.time()
 
@@ -43,47 +42,36 @@ for id, corpus in enums:
     print(ids + " = " + corpus)
 
     # 1.147646188735962
-    corpus_embedding = model.encode(corpus, convert_to_tensor=True)
+    corpus_embedding = model.encode(corpus, convert_to_tensor=True).tolist()
+    #print(corpus_embedding)
 
-    # 0.6244969367980957
-    #corpus_embedding = torch.load( './local_data/' + ids)
-
-    corpus_embeddings.append(corpus_embedding)    
-    corpus_ids.append(ids)
     operation_info = qdrantClient.upsert(
-        collection_name="test_collection",
+        collection_name="text_data",
         wait=True,
         points=[PointStruct(id=id, vector=corpus_embedding, payload={"corpus": corpus})]
     )
-    
-    #torch.save(corpus_embedding, './local_data/' + ids)
 
 end = time.time()
 print("=> execute ",end - start)
 
 # Query sentences:
-queries = ['Is love everywhere ?','A man is eating pasta.', 'Someone in a gorilla costume is playing a set of drums.', 'A cheetah chases prey on across a field.']
+queries = ['tình yêu ở khắp mọi nơi ?','Một người đàn ông đang ăn mì ống.', 'Ai đó trong trang phục khỉ đột đang chơi một bộ trống.', 'Báo là loài động vật trên nhanh nhất hành tinh.']
 
 # Find the closest 5 sentences of the corpus_list for each query sentence based on cosine similarity
 top_k = min(5, len(corpus_list))
 for query in queries:
-    query_embedding = model.encode(query, convert_to_tensor=True)
+    query_embedding = model.encode(query, convert_to_tensor=True).tolist()
 
     print("\n\n======================\n\n")
     print("Query:", query)
     print("\nTop 5 most similar sentences in corpus_list:")
 
     # Alternatively, we can also use util.semantic_search to perform cosine similarty + topk
-    hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=top_k)
-    hits = hits[0]      #Get the hits for the first query
-    for hit in hits:
-        corpus_id = hit['corpus_id']
-        print("corpus_id: ", corpus_id)
-        print(corpus_list[corpus_id], "(Score: {:.4f})".format(hit['score']))
-
     search_result = qdrantClient.search(
-        collection_name="test_collection",
+        collection_name="text_data",
         query_vector=query_embedding, 
         limit=3
     )
-    print(search_result)
+    
+    for rs in search_result:
+        print(rs)
